eb6478d7fc09a0c40f565a73d078b8ff08927e80
张中贵 <89518224+zhang-zhonggui@users.noreply.github.com>
2021-12-20 09:31:12 +0800

    123

---
+++ src/main/java/com/zzg/meven/entiey/Student.java
@@ -0,0 +1,12 @@
+package com.zzg.meven.entiey;
+
+import java.io.Serializable;
+
+/**
+ * @Author: zzg
+ * @Description: TODO
+ * @DateTime: 2021/11/27 9:58
+ */
+public class Student implements Serializable {
+
+}
